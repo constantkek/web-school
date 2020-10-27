fd0295a9d47e1ea3529d2d9057efd0d527ab80a4
Konstantin <kkletsun@gmail.com>
2020-10-27 18:25:59 +0300

    Server first commit

---
+++ server-school/src/main/java/com/example/serverschool/ServerSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSchoolApplication.class, args);
+	}
+
+}
---
+++ server-school/src/test/java/com/example/serverschool/ServerSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
