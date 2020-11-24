d4366ba045da4aab1f1a94bac3e52e90e62f1781
Amil <Limash76912@mail.ru>
2020-11-24 14:52:30 +0300

    express backend added

--- client-school/src/pages/Authorization/Authorization.jsx
+++ client-school/src/pages/Authorization/Authorization.jsx
@@ -6,26 +6,25 @@ export default function Authorization() {
     <div className="authorization">
       <div className="authorization_window">
         <div className="authorization_window_header">
-          <h3>Sign in</h3>
+          <h3>Вход в систему</h3>
         </div>
         <form className="authorization_form" action="">
           <input
             className="authorization_form_login"
             type="text"
-            placeholder="Username"
+            placeholder="Логин"
           />
           <input
             className="authorization_form_password"
             type="password"
-            placeholder="Password"
+            placeholder="Пароль"
           />
-          <button className="authorization_form_btn">Sign in</button>
+          <button className="authorization_form_btn">Войти в систему</button>
           <div className="authorization_form_register">
             <div>
               <p>
                 <span>Если у вас нет аккаунта</span>
               </p>
-
               <p>
                 <a href="#">Свяжитесь с нами</a>
               </p>
