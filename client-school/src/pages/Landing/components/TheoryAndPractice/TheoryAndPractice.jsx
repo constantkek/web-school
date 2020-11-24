1616c1cf8158b92852d63ecd9cf80d8159c1e65e
Konstantin <kkletsun@gmail.com>
2020-11-24 15:06:07 +0300

    Landing finished

--- client-school/src/pages/Landing/components/TheoryAndPractice/TheoryAndPractice.jsx
+++ client-school/src/pages/Landing/components/TheoryAndPractice/TheoryAndPractice.jsx
@@ -12,7 +12,24 @@ export default function TheoryAndPractice() {
         <button className="btn-card" style={{left: '1156px', width: '99px'}}><p className="p-btn-text">C#</p></button>
       </section>
       <section className="main-section">
-
+        <h1 className="h1-tap">Не только теория, но и практика</h1>
+        <p className="p-under-tap">Наши курсы построены по специальной
+          методике и благодаря практическим заданиям
+          на курсах, материал будет усваиваться
+          гораздо лучше, чем на обычных курсах или
+          с помощью книги</p>
+        <div className="div-start-learing" id="learning-first">
+          <h3 className="h3-tasks">Задания</h3>
+          <p className="under-tasks">При выполнении которых вы будете прокачивать свои скиллы</p>
+          <button className="btn-start-learning"><p className="text-btn-learning">Начать обучение</p></button>
+        </div>
+        <div className="div-start-learing" id="learning-second">
+          <h3 className="h3-tasks">Удобная платформа</h3>
+          <p className="under-tasks">Платформа имеет удобный интерфейс,
+            отчего Ваше обучение пройдет
+            максимально эффективно</p>
+          <button className="btn-start-learning"><p className="text-btn-learning">Начать обучение</p></button>
+        </div>
       </section>
     </section>
   )
